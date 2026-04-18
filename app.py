8c0000f6e28419b5c342b1a508197d08bde06d8b
melsonvictory110-bit <melsonvictory110@gmail.com>
2026-04-19 00:14:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,6 @@ def process_frame():
     except Exception as e:
         return jsonify({"status": "SYSTEM ERROR", "authenticated": False})
 
-if _name_== '__main__':
+if __name__== '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
